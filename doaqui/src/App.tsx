63864adbc3ee7f973ec42bde224c57b24bdbbf5c
RenataNune <renatan.sil97@gmail.com>
2022-06-08 14:20:35 -0300

    @RenataNune [Task: Refactory code] {08/06/2022 14:20}

--- doaqui/src/App.tsx
+++ doaqui/src/App.tsx
@@ -1,37 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/statics/navbar/Navbar';
-import Footer from './components/statics/footer/Footer';
-import CadastroDoacoes from './pages/cadastroDoacoes/CadastroDoacoes';
-import Doacoes from './pages/doacoes/Doacoes';
-import Login from './pages/login/Login';
 import Home from "./pages/home/Home"
 import './App.css';
 
-import CadastroUsuario from './pages/cadastroUsuario/CadastroUsuario';
-
 
 function App() {
-  return(
-    <Router>
-      <Navbar />
-      <div style={{ minHeight: '100vh' }}>
-        <Routes> // Antigo Switch
+  return (
+    <main id='app'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
           
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/doacoes" element={<Doacoes />} />
-          
-          <Route path="/cadastroDoacoes" element={<CadastroDoacoes />} />
-
-          <Route path="/cadastroUsuario" element={<CadastroUsuario />} />
-          
-
         </Routes>
-      </div>
-      <Footer />
-    </Router>
+      </Router>
+      <img id='background' src="https://i.imgur.com/vcW2lYW.png" alt="Back" />
+    </main>
   );
 }
 
