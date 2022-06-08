ab2f69e0d18b02b371f4d44f5a98c596a1550f48
GustavoCarvalho25 <gustavo.carvalhoas@hotmail.com>
2022-06-08 17:32:23 -0300

    @GustavoCarvalho25 [Task: Criação de meu perfil] {08/06/2022 17:32}

---
+++ doaqui/src/pages/meuPerfil/Perfil.tsx
@@ -0,0 +1,22 @@
+import React, { ChangeEvent, useEffect, useState } from "react";
+import Footer from '../../components/statics/footer/Footer';
+import Navbar from '../../components/statics/navbar/Navbar';
+import './Perfil.css';
+
+function Perfil() {
+    return (
+        <>
+            <Navbar />
+
+            <div className="perfil-container">
+                <h1>
+                    AAAAAAAAAAAA
+                </h1>
+            </div>
+            
+            <Footer />
+        </>
+    );
+}
+
+export default Perfil;
