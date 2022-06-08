82741de864e00bd858b2d36961cb721accd1e2a7
GustavoCarvalho25 <gustavo.carvalhoas@hotmail.com>
2022-06-08 14:34:17 -0300

    @GustavoCarvalho25 [Task: Refactory code] {08/06/2022 14:34}

---
+++ doaqui/src/components/statics/fazemos/Fazemos.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import './Fazemos.css';
+
+function Fazemos() {
+    return (
+        <>
+            <main id="como_funciona">
+                <header id="banner_title">
+                    <h1>Como nós fazemos</h1>
+                    <h1 id="acontecer">isso acontecer?</h1>
+                </header>
+                <section id="banner_img">
+                    <img src="https://i.imgur.com/PNN4A1e.png" alt="Fazemos" />
+                </section>
+                <footer>
+                    <p>
+                    Somos uma plataforma que reúne diversas grandes empresas que buscam fazer o bem, coletando as notas de doações de maneira simples e dinâmica, evitando burocracias desnecessárias. A partir disso, fornecemos essa gama de doações às nossas ONG's, permitindo que ela escolha aquelas que melhor atendem às suas necessidades. Dessa maneira, transformamos vidas com esse método dedicado ao combate à fome.
+                    </p>
+                </footer>
+            </main>
+        </>
+    );
+}
+
+export default Fazemos;
