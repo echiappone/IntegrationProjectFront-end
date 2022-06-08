be4d32ba2cf16756666fa05c396ddefc67d048aa
GustavoCarvalho25 <gustavo.carvalhoas@hotmail.com>
2022-06-08 15:02:11 -0300

    @GustavoCarvalho25 [Task: Refactory code] {08/06/2022 15:02}

--- doaqui/src/components/statics/fazemos/Fazemos.tsx
+++ doaqui/src/components/statics/fazemos/Fazemos.tsx
@@ -12,11 +12,6 @@ function Fazemos() {
                 <section id="banner_img">
                     <img src="https://i.imgur.com/PNN4A1e.png" alt="Fazemos" />
                 </section>
-                <footer>
-                    <p>
-                    Somos uma plataforma que reúne diversas grandes empresas que buscam fazer o bem, coletando as notas de doações de maneira simples e dinâmica, evitando burocracias desnecessárias. A partir disso, fornecemos essa gama de doações às nossas ONG's, permitindo que ela escolha aquelas que melhor atendem às suas necessidades. Dessa maneira, transformamos vidas com esse método dedicado ao combate à fome.
-                    </p>
-                </footer>
             </main>
         </>
     );
