6a519f6d6f460ef748ca121b6a2b46b27a6d41f6
echiappone <erick_braga_11@hotmail.com>
2022-06-11 19:50:10 -0300

    @echiappone [Task: RESOLVE responsividade NavBar e Login] {11/06/2022 19:49}

--- doaqui/src/pages/login/Login.tsx
+++ doaqui/src/pages/login/Login.tsx
@@ -1,5 +1,6 @@
 import React, { ChangeEvent, useEffect, useState } from "react";
-import { Grid, Typography, TextField, Button } from '@material-ui/core';
+import { Grid, Typography, TextField, Button, InputBase } from '@material-ui/core';
+import { borders } from '@material-ui/system';
 import { Box } from "@mui/material";
 import { Link, useNavigate } from 'react-router-dom';
 import useLocalStorage from "react-use-localstorage";
@@ -60,36 +61,95 @@ function Login() {
     }, [token, navigate]);
 
     return (
-        /*tem que adicionar o navbar*/
         <>
-        
-             <div className="containerPrincipal">
-                <div className="box">
-                    <div className="img-box">
-                        / <img src={'#'} alt="logo" /> aqui nesse campo vai o logo/
-                    </div>
-                    <div className="form-box">
-                        <h2>Login</h2>
-                        <form action="#">
-                            <div className="input-group">
+            <NavbarErick />
+            <Grid container direction='row' justifyContent='center' alignItems='center'>
+                <Grid alignItems='center' xs={6}>
+                    <Box paddingX={20}>
+                        <form onSubmit={onSubmit}>
+                            <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Login</Typography>
+
+                            {/* ****ANTIGO EMAIL****
+                            
+                            <TextField className="input-group"
+                                value={usuario.email}
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                                label='email' variant='outlined' name='email' margin='normal' fullWidth />
+                            */}
+
+                            {/* ****ANTIGA SENHA****
+
+                            <TextField className="input-group"
+                                value={usuario.senha}
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                                id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth/>
+                            */}
+
+                            <div className="input-group" onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}>
                                 <label>E-mail</label>
                                 <input type="email" id="email" placeholder="Digite o seu email" />
                                 <div id="txtEmail"></div>
                             </div>
-                            <div className="input-group w50">
+
+                            <div className="input-group w50" onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}>
                                 <label>Senha</label>
                                 <input type="password" id="senha" placeholder="Digite sua senha" />
                             </div>
-                            <div className="input-group">
-                                <button>Login</button>
-                            </div>
-
+                            
+                            <Box marginTop={2} textAlign='center' className="input-group">
+                                <Button type='submit' variant='contained' color='primary'>
+                                    Logar
+                                </Button>
+                            </Box>
                         </form>
-                    </div>
+                        
+                        <Box display='flex' justifyContent='center' marginTop={2}>
+                            <Box marginRight={1}>
+                                <Typography variant='subtitle1' gutterBottom align='center' className='label1'>Não tem uma conta?</Typography>
+                            </Box>
+                            <Link to='/cadastrousuario'>
+                                <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
+                            </Link>
+                        </Box>
+                    </Box>
+                </Grid>
+                <Grid xs={6} className='imagem'></Grid>
+            </Grid>
+            <Footer />
+
+
+        {/*
+            <NavbarErick />
+
+
+            <div className="containerPrincipal">
+
+            
+                <div className="form-box">
+                    <h2>Login</h2>
+                    <form action="#">
+                        <div className="input-group">
+                            <label>E-mail</label>
+                            <input type="email" id="email" placeholder="Digite o seu email" />
+                            <div id="txtEmail"></div>
+                        </div>
+                        <div className="input-group w50">
+                            <label>Senha</label>
+                            <input type="password" id="senha" placeholder="Digite sua senha" />
+                        </div>
+                        <div className="input-group">
+                            <button>Login</button>
+                        </div>
+
+                    </form>
+
                 </div>
+            
+
             </div>
+            <Footer />
+            */}
         </>
     );
 }
-
 export default Login;
