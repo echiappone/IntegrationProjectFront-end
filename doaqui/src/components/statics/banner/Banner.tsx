1fba21ce7f2b8a92e7ecb5cd4557a6eb1df604d7
GustavoCarvalho25 <gustavo.carvalhoas@hotmail.com>
2022-06-11 22:36:38 -0300

    @GustavoCarvalho25 [Task: Manutenção pagina Home] {11/06/2022 22:36}

--- doaqui/src/components/statics/banner/Banner.tsx
+++ doaqui/src/components/statics/banner/Banner.tsx
@@ -7,7 +7,7 @@ function Banner() {
             <main id="banner">
                 <section id="banner_txt">
                     <h1>Quem tem fome </h1>
-                    <h1>tem <span id='span-borda'>pressa.</span> </h1>
+                    <h1>tem <span className="span-borda">pressa.</span> </h1>
                     <section id="banner_txt_conteudo">
                         <p>Fome extrema atinge quase 200 milhões de pessoas no mundo</p>
                         <p>Conectamos empresas que querem doar rapidamente alimentos a ONGs que distribuem refeições a quem mais precisa</p>
--- doaqui/src/components/statics/contribuir/Contribuir.tsx
+++ doaqui/src/components/statics/contribuir/Contribuir.tsx
@@ -7,7 +7,7 @@ function Contribuir() {
             <main id="quero_doar">
                 <header>
                     <h1>Adorei! Quero contribuir</h1>
-                    <h1 id="passos">Quais são meus proximos passos?</h1>
+                    <h2 id="h2_passos">Quais são meus <span id="span-borda-contribuir">proximos passos?</span></h2>
                 </header>
                 <section>
                     <article>
