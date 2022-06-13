84f64c6b963924d320be5b84b6fd8d6c7474471e
Naomysantana <102303751+Naomysantana@users.noreply.github.com>
2022-06-13 15:42:38 -0300

    @Naomysantana

--- doaqui/src/pages/login/Login.tsx
+++ doaqui/src/pages/login/Login.tsx
@@ -16,46 +16,52 @@ import { toast } from 'react-toastify';
 
 function Login() {
 
-    let navigate = useNavigate();
-    const dispatch = useDispatch();
-    const [token, setToken] = useState('');
-    const [idCriador, setIdCriador] = useLocalStorage('id');
+        let navigate = useNavigate();
+        const dispatch = useDispatch();
+        const [token, setToken] = useState('');
+        const [idCriador, setIdCriador] = useLocalStorage('id');
 
-    const [usuario, setUsuario] = useState<Usuario>(
-        {
-            id: 0,
-            nome: "",
-            email: "",
-            senha: "",
-            telefone: "",
-            endereco: "",
-            tipo: "NORMAL"
-        }
-    );
-
-    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
-
-        setUsuario({
-            ...usuario,
-            [e.target.name]: e.target.value
-        })
-    }
-
-    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
-
-        e.preventDefault();
-            try{
-                await login(`/usuarios/logar`, Login, setToken)
-                toast.success('Usuário logado com sucesso!', {
-                    position: "top-right",
-                    autoClose: 2000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: false,
-                    draggable: false,
-                    theme: "colored",
-                    progress: undefined,
-                    });
+        const [userLogin, setUserLogin] = useState<Usuario>(
+            {
+                id: 0,
+                nome: "",
+                email: "",
+                senha: "",
+                telefone: "",
+                endereco: "",
+                tipo: "NORMAL"
+            }
+            )
+    
+            function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+    
+                setUserLogin({
+                    ...userLogin,
+                    [e.target.name]: e.target.value
+                })
+            }
+    
+                useEffect(()=>{
+                    if(token != ''){
+                        dispatch(addToken(token));
+                        navigate('/meuPerfil')
+                    }
+                }, [token])
+    
+            async function onSubmit(e: ChangeEvent<HTMLFormElement>){
+                e.preventDefault();
+                try{
+                    await login(`/usuarios/logar`, userLogin, setToken)
+                    toast.success('Usuário logado com sucesso!', {
+                        position: "top-right",
+                        autoClose: 2000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: false,
+                        draggable: false,
+                        theme: "colored",
+                        progress: undefined,
+                        });
                 }catch(error){
                     toast.error('Dados do usuário inconsistentes. Erro ao logar!', {
                         position: "top-right",
@@ -70,41 +76,14 @@ function Login() {
                 }
             }
     
-
-    useEffect(() => {
-
-        if (token !== '') {
-            dispatch(addToken(token));
-            navigate('/home');
-        }
-
-    }, [token, navigate]);
-
-    return (
-        <>
+        return (
+            <>
             <NavbarPages />
             <Grid container direction='row' justifyContent='center' alignItems='center'className="gridprincipal">
                 <Grid alignItems='center' xs={6}>
                     <Box className="containerPrincipal" paddingX={20} >
                         <form className="form-box" onSubmit={onSubmit}>
                             <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Login</Typography>
-
-                            {/* ****ANTIGO EMAIL****
-                            
-                            <TextField className="input-group"
-                                value={usuario.email}
-                                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
-                                label='email' variant='outlined' name='email' margin='normal' fullWidth />
-                            */}
-
-                            {/* ****ANTIGA SENHA****
-
-                            <TextField className="input-group"
-                                value={usuario.senha}
-                                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
-                                id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth/>
-                            */}
-
                             <div className="input-group" onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}>
                                 <label>E-mail</label>
                                 <input type="email" id="email" placeholder="Digite o seu email" />
@@ -136,40 +115,8 @@ function Login() {
                 <Grid xs={6} className='imagem'></Grid>
             </Grid>
             <Footer />
-
-
-            {/*
-            <NavbarErick />
-
-
-            <div className="containerPrincipal">
-
-            
-                <div className="form-box">
-                    <h2>Login</h2>
-                    <form action="#">
-                        <div className="input-group">
-                            <label>E-mail</label>
-                            <input type="email" id="email" placeholder="Digite o seu email" />
-                            <div id="txtEmail"></div>
-                        </div>
-                        <div className="input-group w50">
-                            <label>Senha</label>
-                            <input type="password" id="senha" placeholder="Digite sua senha" />
-                        </div>
-                        <div className="input-group">
-                            <button>Login</button>
-                        </div>
-
-                    </form>
-
-                </div>
-            
-
-            </div>
-            <Footer />
-            */}
-        </>
+            </>
     );
 }
-export default Login;
+    
+export default Login ;
