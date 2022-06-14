1ecccef490a557cc84f2a571a5baddad242eebfb
RenataNune <renatan.sil97@gmail.com>
2022-06-14 11:53:29 -0300

    @RenataNune [Task: ADD Carrossel e Poema] {14/06/2022 11:47}

---
+++ doaqui/src/components/statics/poema/Poema.tsx
@@ -0,0 +1,28 @@
+import { BrokenImage } from "@material-ui/icons";
+import React from "react";
+import './Poema.css';
+
+function Poema() {
+    return (
+        <>
+         <p>Vi ontem um bicho
+           Na imundície do pátio
+           Catando comida entre os detritos.
+
+           Quando achava alguma coisa,
+           Não examinava nem cheirava:
+           Engolia com voracidade.
+
+           O bicho não era um cão,
+           Não era um gato,
+           Não era um rato.
+
+          O bicho, meu Deus, era um homem.
+
+          O Bicho, de Manuel Bandeira
+          </p>
+          <img src="https://images.pexels.com/photos/4868573/pexels-photo-4868573.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Foto do poema"
+       
+       </>
+    );
+}
