7a4968ab6249df5014dd91839fb2efa92f476e12
echiappone <erick_braga_11@hotmail.com>
2022-06-15 04:54:34 -0300

    @echiappone [Task: ADD doações] {15/06/2022 04:53}

--- doaqui/src/components/doacoes/tabdoacao/TabDoacao.tsx
+++ doaqui/src/components/doacoes/tabdoacao/TabDoacao.tsx
@@ -4,9 +4,12 @@ import { TabContext, TabPanel } from '@material-ui/lab';
 import { Box } from "@mui/system";
 import ListaPostagem from '../listadoacao/ListaDoacao';
 import './TabDoacao.css';
+import amber from '@mui/material/colors/amber';
 
 
 function TabDoacao() {
+
+    const primary = amber[800]; //#ff8f00
 
     const [value, setValue] = useState('1')
 
@@ -16,18 +19,20 @@ function TabDoacao() {
 
     return (
         <>
-            <TabContext value={value}>
-                <AppBar position="static">
-                    <Tabs centered indicatorColor="secondary" onChange={handleChange}>
-                        <Tab label="Todas as doacoes" value="1" />
-                    </Tabs>
-                </AppBar>
-                <TabPanel value="1" >
-                    <Box display="flex" flexWrap="wrap" justifyContent="center">
-                        <ListaPostagem />
-                    </Box>
-                </TabPanel>
-            </TabContext>
+            <div className='largura'>
+                <TabContext value={value}>
+                    <AppBar position="static">
+                        <Tabs centered indicatorColor="secondary" onChange={handleChange}>
+                            <Tab label="Todas as doacoes" value="1"/>
+                        </Tabs>
+                    </AppBar>
+                    <TabPanel value="1" >
+                        <Box display="flex" flexWrap="wrap" justifyContent="center">
+                            <ListaPostagem />
+                        </Box>
+                    </TabPanel>
+                </TabContext>
+            </div>
         </>
     );
 }
---
+++ doaqui/src/models/DoacoesDTO.ts
@@ -0,0 +1,9 @@
+interface DoacoesDTO 
+{
+    titulo: string;
+    quantidade: number;
+    descricao: string 
+    validade: string;
+}
+
+export default DoacoesDTO;
