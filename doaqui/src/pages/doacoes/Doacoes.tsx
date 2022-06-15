3018290f8729f4bc42a1a0e37c6445c93b5364d6
GustavoCarvalho25 <gustavo.carvalhoas@hotmail.com>
2022-06-15 01:27:12 -0300

    @GustavoCarvalho25 [Task: criando pagina doações] {15/06/2022 01:27}

--- doaqui/src/pages/doacoes/Doacoes.tsx
+++ doaqui/src/pages/doacoes/Doacoes.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Footer from '../../components/statics/footer/Footer';
 import NavbarPages from '../../components/statics/navbarPages/NavbarPages';
+import BarraPesquisa from "../../components/statics/barraPesquisa/BarraPesquisa";
+import './Doacoes.css';
+
+
 
 function Doacoes() {
   return (
     <>
       <NavbarPages />
-      <div>Doacoes</div>
+      <div className="container">
+        <BarraPesquisa />
+      </div>
       <Footer />
     </>
 
